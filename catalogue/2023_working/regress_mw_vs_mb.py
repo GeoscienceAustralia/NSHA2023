7f82223077ceaa76b8c2609e225e4953805b5856
treviallen <trevor.allen@ga.gov.au>
2023-04-23 22:48:27 +1000

    updating regress_mw_vs_mb.py

--- catalogue/2023_working/regress_mw_vs_mb.py
+++ catalogue/2023_working/regress_mw_vs_mb.py
@@ -95,7 +95,7 @@ mwref = delete(mwref, idx)
 # add data from recent events
 ###############################################################################
 # events: 2021 Marble Bar, 2022 Arthur River
-newmb = array([5.699, 4.9, 4.9])
+newmb = array([5.699, 4.9, 4.9]) # last 2 from USGS
 newmw = array([5.323, 4.5, 4.69268914])
 newdt = array([UTCDateTime('2021-11-13T13:05:52.663'), UTCDateTime('2022-01-24T21:24:47.666'), \
               UTCDateTime('2023-04-17T09:02:57.386')])
