3f5898fbd9b724228a2b84818c207618db9a0250
Jonathan Griffin <jdg547@gadi-login-02.gadi.nci.org.au>
2023-05-11 10:43:54 +1000

    Updating NCI run scripts

--- source_models/smoothed_seismicity/run_combine_ss.sh
+++ source_models/smoothed_seismicity/run_combine_ss.sh
@@ -5,24 +5,18 @@
 #PBS -l mem=32GB
 #PBS -l wd
 
-module load intel-cc/12.1.9.293
-module load intel-fc/12.1.9.293
-module load geos/3.5.0
-module load hdf5/1.8.10
-#module load openmpi/1.6.3
-module load gdal
-module unload python
-module load python/2.7.11
-module load python/2.7.11-matplotlib
+module load geos/3.8.0                                                                                                   
+module load hdf5/1.10.7
+module load openmpi/4.1.4                                                                                                  
+module load python3/3.9.2
+# Load gdal after python to avoid conflict                                                                                
+module load gdal/3.5.0
 
-# To get rtree to run
-export SPATIALINDEX_C_LIBRARY=/short/n74/jdg547/spatialindex-src-1.8.5/lib/libspatialindex_c.so.4
-export LD_LIBRARY_PATH=/short/n74/jdg547/spatialindex-src-1.8.5/lib:$LD_LIBRARY_PATH
-# Python paths for local openquake installs and dependencies
-export PYTHONPATH=.:/home/547/jdg547/.local/lib/python2.7/site-packages:${PYTHONPATH}
-export PYTHONPATH=.:/short/w84/NSHA18/sandpit/jdg547/oq-hazardlib:${PYTHONPATH}
-export PYTHONPATH=.:/short/w84/NSHA18/sandpit/jdg547/oq-engine:${PYTHONPATH}
-export PYTHONPATH=.::/short/w84/NSHA18/sandpit/jdg547/NSHA2018/:${PYTHONPATH}
-export PYTHONPATH=.::/short/w84/NSHA18/sandpit/jdg547/:${PYTHONPATH}
 
-python combine_ss_models.py >& combine_ss_models.log 
+# Local pythonpaths                                                                                                        
+export PYTHONPATH=.:/home/547/jdg547/.local/lib/python3.9/site-packages:${PYTHONPATH}
+export PYTHONPATH=.::/home/547/jdg547/:${PYTHONPATH}
+export PYTHONPATH=.::/home/547/jdg547/modelling/oq-engine/:${PYTHONPATH}
+
+python3 combine_ss_frankel_models.py >& combine_ss_frankel_models.log
+#python3 combine_ss_models.py >& combine_ss_models.log         
