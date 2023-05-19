cec2984949146836787b7fb37183ae18ded098da
treviallen <trevor.allen@ga.gov.au>
2023-05-19 13:45:08 +1000

    updating template files

--- jobs/run_oq_model_high_mem.py
+++ jobs/run_oq_model_high_mem.py
@@ -25,11 +25,7 @@ except TypeError:
     raise Exception(('params file %s not found' % args.param_file))
 for line in f_in.readlines():
     row = line.split('=')
-    print(row)
-    try:
-        params[row[0].rstrip()] = row[1].rstrip().lstrip()
-    except:
-        print(params)
+    params[row[0].rstrip()] = row[1].rstrip().lstrip()
 
 user = getpass.getuser()
 run_start_time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
