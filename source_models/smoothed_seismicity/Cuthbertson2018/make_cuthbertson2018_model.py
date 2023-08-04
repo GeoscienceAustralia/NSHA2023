d6ae094cc9c83063e6b330e7bc1384561a3947e2
Jonathan Griffin <jdg547@gadi-login-06.gadi.nci.org.au>
2023-08-04 14:18:01 +1000

    Updating code for making openquake inputs

--- source_models/smoothed_seismicity/Cuthbertson2018/make_cuthbertson2018_model.py
+++ source_models/smoothed_seismicity/Cuthbertson2018/make_cuthbertson2018_model.py
@@ -17,8 +17,8 @@ from scipy import interpolate
 import shapefile
 from shapely.geometry import Polygon
 import shapely.geometry
-from hmtk.sources.source_model import mtkSourceModel
-from hmtk.sources.point_source import mtkPointSource
+from openquake.hmtk.sources.source_model import mtkSourceModel
+from openquake.hmtk.sources.point_source import mtkPointSource
 from openquake.hazardlib.source.point import PointSource
 from openquake.hazardlib.geo.point import Point
 from openquake.hazardlib.scalerel.leonard2014 import Leonard2014_SCR
@@ -32,12 +32,12 @@ from openquake.hazardlib.tom import PoissonTOM
 from openquake.hazardlib.sourcewriter import obj_to_node
 from openquake.baselib.node import Node
 try:
-    from tools.nsha_tools import get_field_data, get_shp_centroid
+    from NSHA2023.tools.nsha_tools import get_field_data, get_shp_centroid
 except:
-    print 'Add PYTHONPATH to NSHA18 root directory'
-from source_models.smoothed_seismicity.combine_ss_models import gr2inc_mmax
-from source_models.smoothed_seismicity.utilities import params_from_shp
-from source_models.logic_trees import logic_tree
+    print('Add PYTHONPATH to NSHA18 root directory')
+from NSHA2023.source_models.smoothed_seismicity.combine_ss_models import gr2inc_mmax
+from NSHA2023.source_models.smoothed_seismicity.utilities import params_from_shp
+from NSHA2023.source_models.logic_trees import logic_tree
 
 # Default values - real values should be based on neotectonic domains
 min_mag = 4.5
@@ -48,26 +48,27 @@ trt = 'Non_cratonic'
 #############################################
 # Parse original data
 original_source_data = 'Aus_9_rates_and_area.txt'
-print 'Reading data from %s' % original_source_data
+print('Reading data from %s' % original_source_data)
 data = np.genfromtxt(original_source_data, delimiter = ' ', 
-                     skip_header = 1, dtype = ("|S24", float, float, int))
-#print data
+                     skip_header = 1, dtype='unicode')# ("|S24", float, float, int)) #("|S24", float, float, int))
+print(data)
 lons = []
 lats = []
 #print data['f0']
-for location in data['f0']:
+for location in data[:,0]:
     location = location.split('_')
     lons.append(float(location[1]))
     lats.append(float(location[2]))
-rates = data['f1']/4.
-a_vals = np.log10(data['f1']/4.) # Divide by 4 as cells are overlapping
-b_vals = data['f2']
+rates = data[:,1].astype(float)/4.
+a_vals = np.log10(data[:,1].astype(float)/4.) # Divide by 4 as cells are overlapping
+b_vals = data[:,2].astype(float)
+print('b_vals', b_vals)
 
 ###############################################################################
 # get neotectonic domain number from centroid
 ###############################################################################
 # load domains shp
-domains_shp =  '../../zones/2018_mw/Domains_single_mc/shapefiles/Domains_NSHA18_MFD.shp'
+domains_shp =  '../../zones/2023_mw/Domains_multi_mc/shapefiles/Domains_NSHA23_MFD.shp'
 #os.path.join('..','..','zones','shapefiles','Domains','Domains_NSHA18_single_Mc.shp')
 dsf = shapefile.Reader(domains_shp)
 lt  = logic_tree.LogicTree('../../../shared/seismic_source_model_weights_rounded_p0.4.csv')
@@ -82,9 +83,9 @@ dom_dep  = get_field_data(dsf, 'DEP_BEST', 'float')
 # Build some dictionaries of parameters for each domain
 param_index = {}
 k = 0
-print params
+print(params)
 for dom in params:
-    print 'Processing source %s' % dom['CODE']
+    print('Processing source %s' % dom['CODE'])
     if dom['TRT'] == 'NCratonic' or dom['TRT'] == 'Extended':
         dom['TRT'] = 'Non_cratonic'
         # For the moment, only consider regions within AUstralia
@@ -92,7 +93,7 @@ for dom in params:
             dom['TRT'] == 'Oceanic' or \
             dom['TRT'] == 'Intraslab' or dom['CODE'] == 'NECS' or \
             dom['CODE'] == 'NWO': 
-        print 'Source %s not on continental Australia, skipping' % dom['CODE']
+        print('Source %s not on continental Australia, skipping' % dom['CODE'])
         k+=1
         continue
     elif dom['TRT'] == 'Cratonic':
@@ -111,13 +112,13 @@ for dom in params:
     k+=1  
 
     if dom['DEP_BEST'] < 0:
-        print 'Setting best depth to 10 km'
+        print('Setting best depth to 10 km')
         dom['DEP_BEST']=10
     if dom['DEP_UPPER'] < 0:
-        print 'Setting upper depth to 5 km'
+        print('Setting upper depth to 5 km')
         dom['DEP_UPPER']=5
     if dom['DEP_LOWER'] < 0:
-        print 'Setting lower depth to 15 km'
+        print('Setting lower depth to 15 km')
         dom['DEP_LOWER']=15
     hypo_depth_dist = PMF([(0.5, dom['DEP_BEST']),
                            (0.25, dom['DEP_LOWER']),
@@ -142,12 +143,12 @@ for dom in params:
                             (0.08, NodalPlane(strikes[4], 30, 90)),
                             (0.08, NodalPlane(strikes[5], 30, 90))])
     if dom['CODE'] == 'WARM' or dom['CODE'] == 'WAPM':
-        print 'Define special case for WARM'
+        print('Define special case for WARM')
         nodal_plane_dist = PMF([(0.75, NodalPlane(45, 90, 0)),
                                 (0.125, NodalPlane(strikes[0], 30, 90)),
                                 (0.125, NodalPlane(strikes[1], 30, 90))])
     if dom['CODE'] == 'FMLR':
-        print 'Define special case for FMLR, 0.5 thrust, 0.5 SS'
+        print('Define special case for FMLR, 0.5 thrust, 0.5 SS')
         nodal_plane_dist = PMF([(0.17, NodalPlane(strikes[0], 30, 90)),
                                 (0.17, NodalPlane(strikes[1], 30, 90)),
                                 (0.04, NodalPlane(strikes[2], 30, 90)),
@@ -170,7 +171,7 @@ dom_shapes = dsf.shapes()
 ###############################################################################
 
 # Build sources
-print 'Building point sources'
+print('Building point sources')
 source_list = []
 source_models = [] # Use later for logic tree
 degoff = np.arange(-0.2,0.3,0.1) # loc offset for distributing rates
@@ -199,7 +200,7 @@ degoff = np.arange(-0.2,0.3,0.1) # loc offset for distributing rates
 #a_split = np.log10((10**a_vals) /  25.) # Normalisation
 
 
-print param_index
+print(param_index)
 for j in range(len(lons)):
     identifier = 'RC_' + str(j)
     name = 'Cuthbertson_' + str(j)
@@ -217,7 +218,7 @@ for j in range(len(lons)):
             try:
                 k = param_index[tmp_dom]
             except KeyError:
-                print 'Skipping points in offshore zone %s' % tmp_dom
+                print('Skipping points in offshore zone %s' % tmp_dom)
                 cont = True
                 continue
 #            max_mag = mmax
@@ -267,7 +268,7 @@ for j in range(len(lons)):
 source_model = mtkSourceModel(identifier=0, name='Cuthbertson2018',
                               sources = source_list)
 
-print 'Writing to NRML'
+print('Writing to NRML')
 outbase = 'cuthbertson2018'
 source_model_filename = outbase + '_source_model.xml'
 source_model.serialise_to_nrml(source_model_filename)
@@ -276,7 +277,7 @@ source_models.append(source_model)
 ######################################
 # Now write the source model logic tree file
 ######################################
-print 'Writing logic tree file'
+print('Writing logic tree file')
 newxml = '<?xml version="1.0" encoding="UTF-8"?>\n'
 newxml += '<nrml xmlns:gml="http://www.opengis.net/gml"\n'
 newxml += '      xmlns="http://openquake.org/xmlns/nrml/0.4">\n\n'
